069ec9c399da52edd53dc05e15a75ee120992997
Kavinkumarm30 <kavinkumar.m30@gmail.com>
2026-08-16 22:39:09 +0530

    perf(android): eliminate startup latency with instant splash routing and configure fast multi-threaded Gradle caching

--- android/app/src/main/java/com/billcraft/studio/ui/SplashActivity.java
+++ android/app/src/main/java/com/billcraft/studio/ui/SplashActivity.java
@@ -2,28 +2,22 @@ package com.billcraft.studio.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import com.billcraft.studio.auth.SessionManager;
-import com.billcraft.studio.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        // Instant startup routing without artificial delay
         SessionManager sessionManager = new SessionManager(this);
-
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            if (sessionManager.isLoggedIn()) {
-                startActivity(new Intent(SplashActivity.this, DashboardActivity.class));
-            } else {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-            }
-            finish();
-        }, 1200);
+        if (sessionManager.isLoggedIn()) {
+            startActivity(new Intent(SplashActivity.this, DashboardActivity.class));
+        } else {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        }
+        finish();
     }
 }
